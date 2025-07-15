60ee22c495a6a15a4ae1a598fc490abcf775d5fa
himanshu <himanshurojwal321@gmail.com>
2025-07-15 22:24:39 +0530

    some and every method

---
+++ file68/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// some and every
+console.log(movements);
+
+// EQUALITY
+console.log(movements.includes(-130));
+
+// SOME: CONDITION
+console.log(movements.some(mov => mov === -130));
+
+const anyDeposits = movements.some(mov => mov > 0);
+console.log(anyDeposits);
+
+// EVERY
+console.log(movements.every(mov => mov > 0));
+console.log(account4.movements.every(mov => mov > 0));
+
+// Separate callback
+const deposit = mov => mov > 0;
+console.log(movements.some(deposit));
+console.log(movements.every(deposit));
+console.log(movements.filter(deposit));
