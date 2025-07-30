dca595eb9cc91e99dbd06c818b9bbf2c882b0bb9
himanshu <himanshurojwal321@gmail.com>
2025-07-30 20:25:56 +0530

    consuming promise

---
+++ file82/script.js
@@ -0,0 +1,74 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+//////////////////////////////////////////////
+
+// Welcome to Callback Hell
+
+const renderCountry = function(data, classname = ''){
+  
+    const html = `
+  <article class="country ${classname}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+        +data.population / 1000000
+      ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  };
+
+  // const getCountryData = function (country){
+  //   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+  //     .then(function(response){
+  //       //console.log(response);
+  //       return response.json();
+  //     })
+  //     .then(function(data){
+  //       //console.log(data);
+  //       renderCountry(data[0]);
+  //     });
+  // };
+  // getCountryData('bharat');
+
+  // const getCountryData = function (country){
+  //   fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+  //     .then(response => response.json())
+  //     .then(data => renderCountry(data[0]));
+    
+  // };
+  // getCountryData('bharat');
+
+/////////////////////////////////////////////////////////////
+// chaining promises
+
+const getCountryData = function (country){
+  //Country 1
+    fetch(`https://countries-api-836d.onrender.com/countries/name/${country}`)
+      .then(response => response.json())
+      .then(data => {
+        renderCountry(data[0]);
+        const neighbour = data[0].borders?.[0];
+
+        if(!neighbour) return;
+
+        //Country 2
+        return fetch(`https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`);
+      })
+      .then(response => response.json())
+      .then(data => renderCountry(data, 'neighbour'));
+    
+  };
+  getCountryData('australia');
+
+
+
